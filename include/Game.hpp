b3bb959e5eda962af648829e0812bf0621989f92
Caleb Emiliano González Arias <a25110172@ceti.mx>
2025-12-07 14:23:30 -0600

    Nivel 1 terminado con cámara y arreglo de buggs

--- include/Game.hpp
+++ include/Game.hpp
@@ -1,13 +1,13 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <box2d/box2d.h>
+#include <vector> 
 
 class Game {
 public:
 //Constructor y Destructor
     Game();
     ~Game();
-
     void run();
 
 private:
@@ -16,14 +16,20 @@ private:
     void update();
     void render();
 
+    void createPlatform(float x, float y, float width, float height);
+
     sf::RenderWindow window;
     b2WorldId worldId;
-
+    
+    //BEN 10
     b2BodyId benBodyId;
     sf::Texture benTexture; // La imagen en memoria
-    sf::Sprite benSprite;   // El dibujo en pantalla
+    sf::Sprite benSprite;   // El dibujo en pantalla 
+
+    //Animación
+    float animationTimer; //Cronometro para cambiar el dibujo
+    int currentFrame; //En que dibujo vamos del 1 al 6 
     
     //Suelo
-    b2BodyId groundId;
-    sf::RectangleShape groundShape;
+    std::vector<sf::RectangleShape> platformShapes; //Para aplicar dibujos para el escenario
 };
--- src/Game.cpp
+++ src/Game.cpp
@@ -9,24 +9,24 @@ Game::Game() {
 
     // 2. Iniciar Física (Box2D v3)
     b2WorldDef worldDef = b2DefaultWorldDef();
-    
     // Gravedad
-    worldDef.gravity = {0.0f, 900.0f}; 
-    
+    worldDef.gravity = {0.0f, 500.0f}; 
     worldId = b2CreateWorld(&worldDef);
 
-    // 3. Crear Suelo
-    b2BodyDef groundDef = b2DefaultBodyDef();
-    groundDef.position = {400.0f, 580.0f};
-    groundId = b2CreateBody(worldId, &groundDef);
+    // 3. Diseñamos el nivel
+    //3.1. El suelo Principal (Que sea Grande y este por debajo)
+    createPlatform(400.0f, 580.0f, 800.0f, 40.0f);
 
-    b2Polygon groundBox = b2MakeBox(400.0f, 10.0f);
-    b2ShapeDef groundShapeDef = b2DefaultShapeDef();
-    b2CreatePolygonShape(groundId, &groundShapeDef, &groundBox);
+    //3.2.Plataformas flotantes(Para hacer PARKOUUUR :D)
+    createPlatform(600.0f, 450.0f, 200.0f, 20.0f); //Derecha baja
+    createPlatform(200.0f, 350.0f, 200.0f, 20.0f); //Izquierda al medio
+    createPlatform(500.0f, 200.0f, 150.0f, 20.0f); //Arriba centro
 
-    groundShape.setSize(sf::Vector2f(800.0f, 20.0f));
-    groundShape.setOrigin(400.0f, 10.0f);
-    groundShape.setFillColor(sf::Color::White);
+    //3.3.Muro alto a la izquierda para que no se salga el ben
+    createPlatform(-10.0f, 300.0f, 20.0f, 600.0f);
+    //3.4.Un muro a la derecha
+    createPlatform(810.0f, 300.0f, 20.0f, 600.0f);
+    
 
     // 4. Crear Cuerpo Físico de Ben (La caja invisible que choca)
     b2BodyDef bodyDef = b2DefaultBodyDef();
@@ -35,7 +35,7 @@ Game::Game() {
     benBodyId = b2CreateBody(worldId, &bodyDef);
     b2Body_SetFixedRotation(benBodyId, true);
 
-    b2Polygon dynamicBox = b2MakeBox(60.0f, 120.0f); // Tamaño ajustado al cuerpo de Ben
+    b2Polygon dynamicBox = b2MakeBox(30.0f, 60.0f); // Tamaño ajustado al cuerpo de Ben
     b2ShapeDef shapeDef = b2DefaultShapeDef();
     shapeDef.density = 1.0f;
     b2CreatePolygonShape(benBodyId, &shapeDef, &dynamicBox);
@@ -64,7 +64,9 @@ Game::Game() {
 
     // D. Escalar (Zoom)
     // Lo hacemos 3 veces más grande para verlo bien.
-    benSprite.setScale(3.0f, 3.0f);
+    benSprite.setScale(1.5f, 1.5f);
+    animationTimer = 0.0f;
+    currentFrame = 0; 
 }
 
 Game::~Game() {
@@ -84,64 +86,137 @@ void Game::processEvents() {
     while (window.pollEvent(event)) {
         if (event.type == sf::Event::Closed)
             window.close();
+        
+        if(event.type == sf::Event::KeyPressed){
+            if(event.key.code == sf::Keyboard::Space){
+                //Obtenemos aqui la velocidad solo para revisar si esta ben en el suelo
+                b2Vec2 velocity = b2Body_GetLinearVelocity(benBodyId);
+
+                //Si la velocidad vertical es baja (esta en el suelo o casi)
+                if (std::abs(velocity.y)<0.5){
+                    velocity.y = -900.0f; //Impulso del salto
+                    b2Body_SetLinearVelocity(benBodyId, velocity);
+                }
+            }
+        }
     }
 }
 
 void Game::update() {
-    //1.Obtener la velocidad del ben
-    b2Vec2 velocity = b2Body_GetLinearVelocity(benBodyId);
+  //1.Obtener la velocidad física de ben 
+  b2Vec2 velocity = b2Body_GetLinearVelocity(benBodyId);
+  bool isMoving = false; //Aplicamos bandera para saber si corremos
 
-    //2.Hacer que se mueva a la DERECHA (con tecla D o flecha Derecha)
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
-        // CORRECCIÓN 2: VELOCIDAD
-        // Antes 5.0f (lento), Ahora 200.0f (rápido)
-        velocity.x = 200.0f; 
-        
-        // CORRECCIÓN 3: ESCALA
-        // Mantenemos el 3.0f del inicio para que no se encoja
-        benSprite.setScale(3.0f, 3.0f);
-    }
-    //3.Hacer que se mueva la la IZQUIERDA(Tacla A o Flecha Izquierda)
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
-        // CORRECCIÓN 4: VELOCIDAD NEGATIVA
-        // Antes -0.5f (casi quieto), Ahora -200.0f (rápido)
-        velocity.x = -200.0f; 
-        
-        // CORRECCIÓN 5: ESCALA NEGATIVA
-        // Mantenemos el -3.0f para voltearlo sin encogerlo
-        benSprite.setScale(-3.0f, 3.0f); 
-    }
-    //4.Frenar(Si no se toca nada)
-    else{
-        velocity.x = 0.0f;
-    }
+  //2.Controles
+  //Vamo a la derecha
+  if(sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+    velocity.x = 200.0f; //Asi aplicamos que se mueva la derecha en el eje x
+    benSprite.setScale(1.5f, 1.5f);
+    isMoving = true; //Aqui aplica que ben se esta moviendo
+  }
+  //Vamo a la Izquierda
+  else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+    velocity.x = -200.0f;//Aplicamos que se mueva a la izquierda en el eje x
+    benSprite.setScale(-1.5f, 1.5f);
+    isMoving = true; // Se esta moviendo a la izquierda :D
+  }
+  //QUIETOO
+  else{
+    velocity.x = 0.0f;
+    isMoving = false;
+  }
+  //3.Sistema de Animación
+  if(isMoving){
+    //3.1.Avanzar el cronómetro(Se suma el tiempo que paso, aproximado de 1/60seg)
+    animationTimer += 1.0f / 60.0f;
+
+    //3.2.Aplicar el cambio de dibujo
+    if(animationTimer >= 0.1f){
+        animationTimer = 0.0f; //Reiniciamos el cronometro
+        currentFrame++; //Siguiente dibujo
     
-    // 5. SALTAR (Tecla Espacio)
-    // Solo salta si la velocidad vertical es casi cero (o sea, si está tocando el suelo)
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && std::abs(velocity.y)< 0.1f){
-        // CORRECCIÓN 6: FUERZA DE SALTO
-        // Antes -12.0f (saltito), Ahora -550.0f (SALTAZO)
-        velocity.y = -550.0f; 
+        //Si llegamos al dibujo 6, regresar al primer dibujo(Loop)
+        if(currentFrame >= 6){
+            currentFrame = 0;
+        }
     }
-    
-    //6.Aplicar la Fisica
-    b2Body_SetLinearVelocity(benBodyId, velocity);
+    //3.3.Calcular el recorte de correr obtenidos en la imagen
+    // Lista exacta de tus coordenadas X:
+        int walkingFramesX[] = {17, 78, 137, 181, 238, 295};
 
-    //Avanzar la simulación
-    b2World_Step(worldId, 1.0f / 60.0f, 4);
+        // Usamos el 'currentFrame' para elegir el número de la lista
+        int frameX = walkingFramesX[currentFrame]; 
 
-    //7.Sincronizar el dibujo
-    b2Vec2 pos = b2Body_GetPosition(benBodyId);
-    benSprite.setPosition(pos.x, pos.y);
+        // Actualizamos el recorte usando la X exacta de la lista
+        // (Y=382, Ancho=47, Alto=79 según tus medidas anteriores)
+        benSprite.setTextureRect(sf::IntRect(frameX, 382, 46.5, 79));
 
-    //Sincronizar tambien el suelo
-    b2Vec2 groundPos = b2Body_GetPosition(groundId);
-    groundShape.setPosition(groundPos.x, groundPos.y);
+        // Ajustamos el origen para el ancho de 47
+        benSprite.setOrigin(23.5f, 39.5f);
+
+  }
+  else{
+    //3.4.Si esta quieto volver al dibujo original 
+    //Datos del dibujo original: X=8, Y=23, Alto= 51, Largo=84 aprox
+    currentFrame = 0; //Resetear para que al correr empiece en el dibujo 1
+    benSprite.setTextureRect(sf::IntRect(8, 23, 51, 84));
+    benSprite.setOrigin(25.5f, 42.0f);
+  }
+  //4.Aplicar las fisicas y sincronizamos
+  b2Body_SetLinearVelocity(benBodyId, velocity);
+  b2World_Step(worldId, 1.0f / 60.0f, 4);
+
+  b2Vec2 pos = b2Body_GetPosition(benBodyId);
+  benSprite.setPosition(pos.x,pos.y);
 }
 
 void Game::render(){
     window.clear(sf::Color::Black); // Limpiar pantalla
-    window.draw(groundShape);       // Dibujar suelo
-    window.draw(benSprite);         // Dibujar Ben
-    window.display();               // Mostrar todo
-} 
+
+    // Obtenemos la posición de Ben
+    sf::Vector2f benPos = benSprite.getPosition();
+
+    // Creamos una "Cámara" del tamaño de nuestra ventana (800x600)
+    sf::View camera(sf::Vector2f(0.0f, 0.0f), sf::Vector2f(800.0f, 600.0f));
+
+    // Le decimos a la cámara: "Mira a Ben"
+    // Por ahora, que lo siga en todo:
+    camera.setCenter(benPos);
+    // Aplicamos la cámara a la ventana
+    window.setView(camera);
+
+    // Dibujar plataformas
+    for (const auto& shape : platformShapes) {
+        window.draw(shape);
+    }
+
+    // Dibujar a Ben
+    window.draw(benSprite);
+    window.display();
+}
+
+//Aplicacion de las plataformas
+void Game::createPlatform(float x, float y, float width, float height){
+//1.Fisicas (Box2D)
+b2BodyDef bodyDef = b2DefaultBodyDef();
+bodyDef.position = {x, y}; //Box2D aplicamos para usar el centro
+b2BodyId bodyId = b2CreateBody(worldId, &bodyDef);
+
+//Box2D pide la mitad del ancho y alto
+b2Polygon box = b2MakeBox(width / 2.0f, height / 2.0f);
+b2ShapeDef shapeDef = b2DefaultShapeDef();
+b2CreatePolygonShape(bodyId, &shapeDef, &box);
+
+//2.SFML (Para aplicar los graficos)
+sf::RectangleShape shape(sf::Vector2f(width, height));
+shape.setOrigin(width / 2.0f, height / 2.0); //Origen al centro igual que Box2D
+shape.setPosition(x, y);
+
+//Aplicamos Color 
+shape.setFillColor(sf::Color(0, 180, 0)); //Verde Oscuro
+shape.setOutlineThickness(2.0f);
+shape.setOutlineColor(sf::Color::White);
+
+//3.Guardamos en la lista para despues dibujarlo
+platformShapes.push_back(shape);
+}
